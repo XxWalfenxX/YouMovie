ed3bef949b26e8cfe68ec317d9a7610c7f780b6b
Walfen <74185307+XxWalfenxX@users.noreply.github.com>
2023-04-11 15:31:50 +0200

    Update firebase-register.js

--- src/firebase/firebase-register.js
+++ src/firebase/firebase-register.js
@@ -1,27 +1,35 @@
-import { auth } from './index.js'
-import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
-import { Loading, Notify } from 'quasar'
+import { auth } from "./index.js";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { Loading, Notify } from "quasar";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "src/firebase/index";
 
 const register = (data) => {
- return new Promise((resolve, reject) => {
-  Loading.show()
+  return new Promise((resolve, reject) => {
+    Loading.show();
 
-  createUserWithEmailAndPassword(auth, data.email, data.password).then(userCredential => {
-   updateProfile(userCredential.user, {
-    displayName: data.name
-   })
+    createUserWithEmailAndPassword(auth, data.email, data.password)
+      .then((userCredential) => {
+        updateProfile(userCredential.user, {
+          displayName: data.name,
+        });
+        setDoc(doc(db, "users", data.email), {
+          admin: false,
+          pelisID: [],
+        });
 
-   Loading.hide()
-   resolve(userCredential.user)
-  }).catch(err => {
-   Loading.hide()
-   Notify.create({
-    type: 'negative',
-    message: err.message
-   })
-   reject(err.message)
-  })
- })
-}
+        Loading.hide();
+        resolve(userCredential.user);
+      })
+      .catch((err) => {
+        Loading.hide();
+        Notify.create({
+          type: "negative",
+          message: err.message,
+        });
+        reject(err.message);
+      });
+  });
+};
 
-export default register
+export default register;
