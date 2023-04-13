fdb43894e1672a396c9a952021f2f0197339da28
Walfen <74185307+XxWalfenxX@users.noreply.github.com>
2023-04-13 19:09:48 +0200

    Update AyudaWeb.vue

--- src/pages/AyudaWeb.vue
+++ src/pages/AyudaWeb.vue
@@ -2,30 +2,37 @@
   <div class="q-pa-md">
     <h3>Ayuda</h3>
     <q-list bordered class="rounded-borders">
-      <q-expansion-item
-        expand-separator
-        label="¿Qué es YouMovie?"
-      >
+      <q-expansion-item expand-separator label="¿Qué es YouMovie?">
         <q-card>
           <q-card-section>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem,
-            eius reprehenderit eos corrupti commodi magni quaerat ex numquam,
-            dolorum officiis modi facere maiores architecto suscipit iste
-            eveniet doloribus ullam aliquid.
+            YouMovie es un servicio de streaming que permite a sus usuarios ver
+            películas a través de su página web o aplicación movil.
           </q-card-section>
         </q-card>
       </q-expansion-item>
 
+      <q-expansion-item expand-separator label="¿Cómo cambio mi contraseña?">
+        <q-card>
+          <q-card-section>
+            Para cambiar tu contraseña, pulsa en el botón de haburguesa, situado
+            en la parte superior izquierda de la pantalla. Se desplegará el menú
+            de usuario <q-icon name="menu" color="primary"></q-icon> y pulsamos
+            en la opción "<q-icon name="person" color="primary"></q-icon> Mi
+            Cuenta". Que nos redigirá a la página de gestión de usuario, en esta
+            página, puslamos sobre el campo de contraseña e introducimos una
+            nueva, para aplicar los cambios pulsamos en el botón Cambiar
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
       <q-expansion-item
         expand-separator
-        label="Wifi settings"
+        label="¿Es necesario una conexión de alta velocidad para reproducir una pelicula?"
       >
         <q-card>
           <q-card-section>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem,
-            eius reprehenderit eos corrupti commodi magni quaerat ex numquam,
-            dolorum officiis modi facere maiores architecto suscipit iste
-            eveniet doloribus ullam aliquid.
+            No, no necesitas una alta velocidad, se recomienda que la velocidad
+            sea de 25mbps, pero puedes ajustar la calidad de la película en los
+            ajustes del reproductor
           </q-card-section>
         </q-card>
       </q-expansion-item>
