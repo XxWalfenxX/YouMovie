752693a063f604a593e6c8df7082cb4555e97d97
Firagon <93060162+firagonb3@users.noreply.github.com>
2023-04-13 20:34:25 +0200

    error

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+  <div class="fullscreen bg-primary text-white text-center q-pa-md flex flex-center">
     <div>
       <div style="font-size: 30vh">
-        404
+        <q-img src="~assets/shiba-error.png"/>
       </div>
 
       <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
+        Ups. Nada aquí... 404
       </div>
 
       <q-btn
         class="q-mt-xl"
         color="white"
-        text-color="blue"
+        text-color="primary"
         unelevated
         to="/"
-        label="Go Home"
+        label="Inicio"
         no-caps
       />
     </div>
