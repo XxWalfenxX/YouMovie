18b1e412cc282a8f28838d69965452a01afde002
Walfen <74185307+XxWalfenxX@users.noreply.github.com>
2023-04-17 20:55:03 +0200

    admin

--- src/pages/AdminPanel.vue
+++ src/pages/AdminPanel.vue
@@ -1,68 +1,70 @@
 <template>
   <div>
-    <q-form @submit="agregarPeli" class="centrado">
-      <div class="row flex-center " style="height: 100%;">
-        <div class="col-4 q-gutter-md">
-        <h4>Agregar pelicula</h4>
-          <q-input
-            name="name"
-            v-model="tmdb"
-            label="ID TMDB"
-            filled
-            clearable
-          />
-          <q-input
-            name="name"
-            v-model="synology"
-            label="Link Synology"
-            filled
-            clearable
-          />
-          <q-btn label="Guardar" type="submit" color="primary" />
-        </div>
-      </div>
-    </q-form>
+    <q-splitter
+      v-model="splitterModel"
+      style="height: 100%"
+    >
+
+      <template v-slot:before>
+        <q-tabs
+          v-model="tab"
+          vertical
+          class="text-primary"
+        >
+          <q-tab name="add" icon="add" label="Agregar Pelicula" />
+          <q-tab name="modify" icon="edit" label="Modificar Pelicula" />
+          <q-tab name="remove" icon="remove" label="Eliminar Pelicula" />
+        </q-tabs>
+      </template>
+
+      <template v-slot:after>
+        <q-tab-panels
+          v-model="tab"
+          animated
+          swipeable
+          vertical
+          transition-prev="jump-up"
+          transition-next="jump-up"
+        >
+          <q-tab-panel name="add">
+            <AgregarPeli/>
+          </q-tab-panel>
+
+          <q-tab-panel name="modify">
+            <div class="text-h4 q-mb-md">Alarms</div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+          </q-tab-panel>
+
+          <q-tab-panel name="remove">
+            <div class="text-h4 q-mb-md">Movies</div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+          </q-tab-panel>
+        </q-tab-panels>
+      </template>
+
+    </q-splitter>
   </div>
 </template>
 
 <script>
 import { defineComponent, ref } from "vue";
-import AdminAuth from "src/firebase/AdminAuth";
-import { useRouter } from "vue-router";
-import { useQuasar } from "quasar";
-
-import CollectionMovie from "src/tmdb/tmdb"
+import AgregarPeli from "src/components/AgregarPeli.vue";
 
 export default defineComponent({
   name: "AdminPanel",
 
+  components:{
+    AgregarPeli,
+  },
+
   setup() {
-    const $q = useQuasar();
-    const user = $q.localStorage.getItem("user");
-    const router = useRouter();
-    AdminAuth(user.email).then((v) => {
-      if (v == false) {
-        router.push("/app");
-      }
-    });
-
     return {
-      tmdb: ref(null),
-      synology: ref(null),
-
-      agregarPeli(evt){
-        console.log(evt);
-        CollectionMovie(evt.target[0].value, evt.target[2].value);
-      }
+      tab: ref('add'),
+      splitterModel: ref(20)
     };
   },
 });
 </script>
-
-
-<style>
-.centrado {
-    height: calc(100vh - 98px);
-}
-
-</style>
