db85be7d5303b358d5c9034212d1a5f09a05f921
Walfen <74185307+XxWalfenxX@users.noreply.github.com>
2023-05-03 20:46:54 +0200

    Update DescipcionPelicula.vue

--- src/pages/DescipcionPelicula.vue
+++ src/pages/DescipcionPelicula.vue
@@ -2,8 +2,10 @@
   <q-img class="img-fondo" :src="state.peliData.imagenFondo">
     <div class="absolute-full pading">
       <div class="column">
+        <!-- Columna 1 -->
         <div class="col">
           <div class="row items-center justify-center-mv">
+            <!-- Logo y botón de "Ver ahora" -->
             <div class="col-md-3 col-8">
               <div class="flex">
                 <q-img class="img-titulo" :src="state.peliData.logo"></q-img>
@@ -21,6 +23,7 @@
                 />
               </div>
             </div>
+            <!-- Descripción y categorías -->
             <div class="col-md-4 col-8">
               <div class="Contenedor-Texto">
                 {{ state.peliData.descripcion }}
@@ -35,7 +38,9 @@
             </div>
           </div>
         </div>
+        <!-- Columna 2 -->
         <div class="col">
+          <!-- Valoración -->
           <div class="Contenedor-Texto valoracion">
             <q-circular-progress
               show-value
@@ -53,6 +58,7 @@
           </div>
         </div>
       </div>
+      <!-- Películas similares -->
       <h4>Similares</h4>
       <q-scroll-area style="height: 28rem; max-width: 100vw">
         <div class="q-pa-lg q-pa-lg-m row items-start scrolllateral">
@@ -74,53 +80,62 @@
 </template>
 
 <script>
-import { reactive } from "vue";
-import { useRoute, useRouter } from "vue-router";
-import TarjetaPeli from "components/TarjetaPeli.vue";
-import getUnaPelicula from "src/firebase/ObtenerUnaPelicula";
-import { useQuasar } from "quasar";
-import { LocalStorage } from "quasar";
+import { reactive } from "vue"; // importa la función reactive de Vue
+import { useRoute, useRouter } from "vue-router"; // importa las funciones useRoute y useRouter de vue-router
+import TarjetaPeli from "components/TarjetaPeli.vue"; // importa el componente TarjetaPeli
+import getUnaPelicula from "src/firebase/ObtenerUnaPelicula"; // importa la función getUnaPelicula desde la ruta especificada
+import { useQuasar } from "quasar"; // importa la función useQuasar de Quasar
+import { LocalStorage } from "quasar"; // importa el objeto LocalStorage de Quasar
 
-import getPeliculas from "src/firebase/ObtenerPeliculas";
+import getPeliculas from "src/firebase/ObtenerPeliculas"; // importa la función getPeliculas desde la ruta especificada
 
 export default {
-  name: "DescipcionPelicula",
+  name: "DescipcionPelicula", // define el nombre del componente
+
   components: {
-    TarjetaPeli,
+    TarjetaPeli, // registra el componente TarjetaPeli dentro de este componente
   },
 
   setup() {
-    const $q = useQuasar();
-    const router = useRouter();
-    const idPeli = useRoute().params.id;
+    // define la función setup
+    const $q = useQuasar(); // asigna el valor de la función useQuasar a la constante $q
+    const router = useRouter(); // asigna el valor de la función useRouter a la constante router
+    const idPeli = useRoute().params.id; // asigna el valor del parámetro id de la ruta actual a la constante idPeli
     const state = reactive({
+      // crea un objeto reativo llamado state con propiedades peliData, valoracion y listaPelis
       peliData: [],
       valoracion: 0,
       listaPelis: [],
     });
 
-    const peliActual = $q.localStorage.getItem("peliActual");
+    const peliActual = $q.localStorage.getItem("peliActual"); // asigna el valor del objeto peliActual guardado en LocalStorage a la constante peliActual
 
     getPeliculas.then((pelis) => {
+      // llama a la función getPeliculas y asigna el resultado a state.listaPelis
       state.listaPelis = pelis;
     });
 
     if (peliActual.id != idPeli || peliActual == null) {
+      // si peliActual no coincide con idPeli o es nulo, llama a la función getUnaPelicula con idPeli como parámetro
       getUnaPelicula(idPeli)
         .then(function (data) {
+          // si la función getUnaPelicula devuelve datos, asigna los datos a state.peliData, redondea data.valoracion y lo asigna a state.valoracion, y guarda el objeto data en LocalStorage
           if (data === null) {
-            router.push("/app");
+            router.push("/app"); // si data es nulo, redirige al usuario a la página /app
           }
-          console.log("Ha pedido info a db");
+          console.log("Ha pedido info a db"); // imprime en la consola "Ha pedido info a db"
           state.peliData = data;
           state.valoracion = Math.round(data.valoracion * 10);
           LocalStorage.set("peliActual", data);
         })
         .catch(function (error) {
+          // si la función getUnaPelicula devuelve un error, imprime el error en la consola
           console.error(error);
         });
     } else {
+      // Si la película actual es igual a la película solicitada, entonces la información ya está en la memoria local.
       console.log("NO pedido info a db");
+      // Se obtiene la información de la memoria local.
       state.peliData = peliActual;
       state.valoracion = Math.round(peliActual.valoracion * 10);
     }
