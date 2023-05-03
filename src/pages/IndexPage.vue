627275f4c52d3126353169fa94b33d62dc4fb813
Firagon <93060162+firagonb3@users.noreply.github.com>
2023-05-03 20:21:12 +0200

    Update IndexPage.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="q-pa-lg">
+    <!-- Inicio del componente Q-Carousel -->
     <q-carousel
       class="border"
       animated
@@ -14,6 +15,7 @@
       @mouseenter="autoplay = false"
       @mouseleave="autoplay = true"
     >
+      <!-- Se generan los slides del carousel -->
       <q-carousel-slide
         v-for="(peli, index) in state.pelisCarrousel"
         :key="index"
@@ -21,11 +23,13 @@
         :img-src="peli.imagenFondo"
         style="overflow: hidden"
       >
+        <!-- Se crea un link hacia la descripción de la película -->
         <router-link
           :to="{ name: 'DescipcionPelicula', params: { id: peli.id } }"
           style="text-decoration: none; color: white"
         >
           <div class="custom-caption">
+            <!-- Se agrega el logo de la película en el caption del slide -->
             <div class="contenedor-logo">
               <q-img
                 :src="peli.logo"
@@ -38,9 +42,11 @@
       </q-carousel-slide>
     </q-carousel>
 
+    <!-- Sección de "Novedades" -->
     <h4>Novedades</h4>
     <q-scroll-area style="height: 28rem; max-width: 100vw">
       <div class="q-pa-lg q-pa-lg-m row items-start scrolllateral">
+        <!-- Se generan las tarjetas de las películas para la sección de "Novedades" -->
         <TarjetaPeli
           v-for="imagen in state.listaPelis.slice(0, 15)"
           :key="imagen.id"
@@ -50,10 +56,12 @@
       </div>
     </q-scroll-area>
 
+    <!-- Secciones de categorías -->
     <div v-for="categoria in state.categorias" :key="categoria">
       <h4>{{ categoria }}</h4>
       <q-scroll-area style="height: 28rem; max-width: 100vw">
         <div class="q-pa-lg q-pa-lg-m row items-start scrolllateral">
+          <!-- Se generan las tarjetas de las películas para cada categoría -->
           <TarjetaPeli
             v-for="imagen in state.listaPelis.filter((peli) =>
               peli.categorias.find((ca) => ca.name === categoria)
@@ -65,6 +73,7 @@
         </div>
       </q-scroll-area>
     </div>
+    <!-- Botón para subir al inicio de la página -->
     <q-page-scroller
       position="bottom-right"
       :scroll-offset="150"
@@ -80,17 +89,18 @@ import { defineComponent, ref, reactive } from "vue";
 import TarjetaPeli from "components/TarjetaPeli.vue";
 import getPeliculas from "src/firebase/ObtenerPeliculas";
 
-// redondear
-//console.log(Math.round(8.351*10))
-
+// Definimos el componente
 export default defineComponent({
   name: "IndexPage",
 
+  // Importamos el componente de tarjeta de película
   components: {
     TarjetaPeli,
   },
 
+  // Configuramos el componente con Vue 3 Composition API
   setup() {
+    // Definimos el estado del componente
     const state = reactive({
       categorias: [
         "Aventura",
@@ -107,13 +117,17 @@ export default defineComponent({
       pelisCarrousel: [],
     });
 
+    // Obtenemos las películas desde Firebase y las asignamos al estado
     getPeliculas.then((pelis) => {
       state.listaPelis = pelis;
+
+      // Configuramos las películas del carrusel
       state.pelisCarrousel = [...pelis]
         .sort(() => Math.random() - 0.5)
         .slice(0, 5);
     });
 
+    // Retornamos las propiedades reactivas y las referencias necesarias
     return {
       state,
       slide: ref(1),
