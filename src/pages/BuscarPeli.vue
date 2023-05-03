2a0163a80b2c7872c8aad98a9fd5e53530430518
Firagon <93060162+firagonb3@users.noreply.github.com>
2023-05-03 20:04:41 +0200

    Update BuscarPeli.vue

--- src/pages/BuscarPeli.vue
+++ src/pages/BuscarPeli.vue
@@ -1,8 +1,10 @@
 <template>
   <div style="margin: 2em">
+    <!-- Barra de búsqueda -->
     <q-input standout v-model="search" placeholder="Titulo" :dense="dense" />
   </div>
   <div class="row justify-center q-gutter-md">
+    <!-- Lista de TarjetaPeli -->
     <TarjetaPeli
       v-for="imagen in state.listaPelis2"
       :key="imagen.id"
@@ -13,32 +15,33 @@
 
 <script>
 import { defineComponent, ref, reactive, watch } from "vue";
-import TarjetaPeli from "components/TarjetaPeli.vue";
-import getPeliculas from "src/firebase/ObtenerPeliculas";
+import TarjetaPeli from "components/TarjetaPeli.vue";  // Importa el componente TarjetaPeli
+import getPeliculas from "src/firebase/ObtenerPeliculas";  // Importa la función para obtener las películas
 
 export default defineComponent({
-  name: "BuscarPeli",
+  name: "BuscarPeli",  // Nombre del componente
 
   components: {
-    TarjetaPeli,
+    TarjetaPeli,  // Se registra el componente TarjetaPeli
   },
 
   setup() {
     const state = reactive({
-      listaPelis: [],
-      listaPelis2: [],
+      listaPelis: [],  // Lista de todas las películas
+      listaPelis2: [],  // Lista de películas filtradas por la barra de búsqueda
     });
 
-    const search = ref("");
+    const search = ref("");  // Barra de búsqueda vacía al principio
 
-    getPeliculas.then((pelis) => {
-      state.listaPelis = pelis;
+    getPeliculas.then((pelis) => {  // Obtiene las películas de Firebase
+      state.listaPelis = pelis;  // Guarda las películas en la lista original
     });
 
-    watch(search, () => {
-      if (search.value === "") {
-        state.listaPelis2 = "";
+    watch(search, () => {  // Observa cambios en la barra de búsqueda
+      if (search.value === "") {  // Si la barra de búsqueda está vacía
+        state.listaPelis2 = "";  // No hay películas filtradas
       } else {
+        // Filtra las películas por el título (ignorando mayúsculas y minúsculas)
         state.listaPelis2 = state.listaPelis.filter((peli) =>
           peli.nombre.toLowerCase().includes(search.value.toLowerCase())
         );
@@ -48,7 +51,7 @@ export default defineComponent({
     return {
       state,
       search,
-      dense: ref(false),
+      dense: ref(false),  // Barra de búsqueda no densa
     };
   },
 });
