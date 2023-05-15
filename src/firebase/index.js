768cbe8b65e8f4b481ea129708cc1676684151cd
Firagon <93060162+firagonb3@users.noreply.github.com>
2023-05-15 19:46:44 +0200

    env

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Configuración de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBcgG1H11irNfOTglZqpjQRA3k3yshOP0M",
-  authDomain: "youmovie-10082.firebaseapp.com",
-  projectId: "youmovie-10082",
-  storageBucket: "youmovie-10082.appspot.com",
-  messagingSenderId: "674100460556",
-  appId: "1:674100460556:web:58aa17e24a4917c01f0229",
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
 };
 
 // Inicializamos Firebase con la configuración anterior
